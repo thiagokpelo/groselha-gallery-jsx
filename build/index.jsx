299b53a41a51120889632f24476157fa55b7456e
Thiago Capelo <thiagokpelo@gmail.com>
2020-02-10 21:30:23 -0300

    fix: jsx preserve

--- build/index.jsx
+++ build/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Container, Image } from './styles';
-var Gallery = function (_a) {
-    var data = _a.data, imageSize = _a.imageSize;
-    return (<Container>
-    {(data || []).map(function (item) { return (<Image key={item.id} alt={item.caption.text} src={item.images.standard_resolution.url} size={imageSize}/>); })}
+const Gallery = ({ data, imageSize }) => (<Container>
+    {(data || []).map((item) => (<Image key={item.id} alt={item.caption.text} src={item.images.standard_resolution.url} size={imageSize}/>))}
   </Container>);
-};
 export default Gallery;
 //# sourceMappingURL=index.jsx.map
--- build/styles.js
+++ build/styles.js
@@ -1,17 +1,20 @@
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
 import styled from 'styled-components';
-var SIZE = 200;
-export var Container = styled.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  box-sizing: border-box;\n  display: flex;\n  flex: 1;\n  flex-direction: row;\n  width: 100%;\n  flex-wrap: nowrap;\n  overflow-x: scroll;\n"], ["\n  box-sizing: border-box;\n  display: flex;\n  flex: 1;\n  flex-direction: row;\n  width: 100%;\n  flex-wrap: nowrap;\n  overflow-x: scroll;\n"])));
+const SIZE = 200;
+export const Container = styled.div `
+  box-sizing: border-box;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  width: 100%;
+  flex-wrap: nowrap;
+  overflow-x: scroll;
+`;
 // eslint-disable-next-line prettier/prettier
-export var Image = styled.img(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  box-sizing: border-box;\n  margin: 0;\n  width: ", "px;\n  height: ", "px;\n  object-fit: cover;\n"], ["\n  box-sizing: border-box;\n  margin: 0;\n  width: ", "px;\n  height: ", "px;\n  object-fit: cover;\n"])), function (_a) {
-    var size = _a.size;
-    return size || SIZE;
-}, function (_a) {
-    var size = _a.size;
-    return size || SIZE;
-});
-var templateObject_1, templateObject_2;
+export const Image = styled.img `
+  box-sizing: border-box;
+  margin: 0;
+  width: ${({ size }) => size || SIZE}px;
+  height: ${({ size }) => size || SIZE}px;
+  object-fit: cover;
+`;
 //# sourceMappingURL=styles.js.map
