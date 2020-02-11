f3a3eb5e0e690f0bf4d8b1e76838072643f6ae9d
Thiago Capelo <thiagokpelo@gmail.com>
2020-02-10 21:19:50 -0300

    fix: module export

--- build/index.js
+++ build/index.js
@@ -1,10 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var styles_1 = require("./styles");
+import React from 'react';
+import { Container, Image } from './styles';
 var Gallery = function (_a) {
     var data = _a.data, imageSize = _a.imageSize;
-    return (react_1.default.createElement(styles_1.Container, null, (data || []).map(function (item) { return (react_1.default.createElement(styles_1.Image, { key: item.id, alt: item.caption.text, src: item.images.standard_resolution.url, size: imageSize })); })));
+    return (React.createElement(Container, null, (data || []).map(function (item) { return (React.createElement(Image, { key: item.id, alt: item.caption.text, src: item.images.standard_resolution.url, size: imageSize })); })));
 };
-exports.default = Gallery;
+export default Gallery;
 //# sourceMappingURL=index.js.map
--- build/styles.js
+++ build/styles.js
@@ -1,14 +1,12 @@
-"use strict";
 var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
     if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
     return cooked;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var styled_components_1 = require("styled-components");
+import styled from 'styled-components';
 var SIZE = 200;
-exports.Container = styled_components_1.default.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  box-sizing: border-box;\n  display: flex;\n  flex: 1;\n  flex-direction: row;\n  width: 100%;\n  flex-wrap: nowrap;\n  overflow-x: scroll;\n"], ["\n  box-sizing: border-box;\n  display: flex;\n  flex: 1;\n  flex-direction: row;\n  width: 100%;\n  flex-wrap: nowrap;\n  overflow-x: scroll;\n"])));
+export var Container = styled.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  box-sizing: border-box;\n  display: flex;\n  flex: 1;\n  flex-direction: row;\n  width: 100%;\n  flex-wrap: nowrap;\n  overflow-x: scroll;\n"], ["\n  box-sizing: border-box;\n  display: flex;\n  flex: 1;\n  flex-direction: row;\n  width: 100%;\n  flex-wrap: nowrap;\n  overflow-x: scroll;\n"])));
 // eslint-disable-next-line prettier/prettier
-exports.Image = styled_components_1.default.img(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  box-sizing: border-box;\n  margin: 0;\n  width: ", "px;\n  height: ", "px;\n  object-fit: cover;\n"], ["\n  box-sizing: border-box;\n  margin: 0;\n  width: ", "px;\n  height: ", "px;\n  object-fit: cover;\n"])), function (_a) {
+export var Image = styled.img(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  box-sizing: border-box;\n  margin: 0;\n  width: ", "px;\n  height: ", "px;\n  object-fit: cover;\n"], ["\n  box-sizing: border-box;\n  margin: 0;\n  width: ", "px;\n  height: ", "px;\n  object-fit: cover;\n"])), function (_a) {
     var size = _a.size;
     return size || SIZE;
 }, function (_a) {
